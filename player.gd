807cef623562cad8ddeccf817159ed72a2caca90
Erik Velf <velf.erik@protonmail.com>
2025-05-25 12:48:43 +0200

    Add player.gd control script

--- player.gd
+++ player.gd
@@ -1,28 +1,39 @@
 extends CharacterBody3D
 
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+var target_velocity = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
+	
+	# Ground velocity, pay attention to Z !!!
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Vertical velocity
+	if not is_on_floor(): # gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	# Moving the character
+	velocity = target_velocity
+	move_and_slide() # Don't stop, slide if possible, very powerful !!!
